33c18c942128e46e6c2e5e1a31d994cbce6a219e
jiawei <jiawei@parim.net>
2016-03-03 18:54:27 +0800

    修改用户密码生成和校验规则，兼容历史遗留系统

--- spark-core/spark-common/src/main/java/net/parim/common/security/PasswordHelper.java
+++ spark-core/spark-common/src/main/java/net/parim/common/security/PasswordHelper.java
@@ -1,11 +1,14 @@
 package net.parim.common.security;
 
+import javax.xml.bind.DatatypeConverter;
+
+
 import net.parim.common.utils.Encodes;
 
 public class PasswordHelper {
-	public static final String HASH_ALGORITHM = "SHA-1";
-	public static final int HASH_INTERATIONS = 1024;
-	public static final int SALT_SIZE = 8;
+	public static final String HASH_ALGORITHM = "MD5";
+	public static final int HASH_INTERATIONS = 1;
+	public static final int SALT_SIZE = 22;
 	
 	public static String generateSalt(){
 		byte[] salt = Digests.generateSalt(SALT_SIZE);
@@ -13,30 +16,30 @@ public class PasswordHelper {
 	}
 	
 	/**
-	 * 生成安全的密码，生成随机的16位salt并经过1024次 sha-1 hash
+	 * 通过随机生成的22位salt并经过1次 md5 hash生成安全的密码，
 	 */
-	public static String entryptPassword(String plainPassword) {
-		byte[] salt = Digests.generateSalt(SALT_SIZE);
-		byte[] hashPassword = Digests.sha1(plainPassword.getBytes(), salt, HASH_INTERATIONS);
-		return Encodes.encodeHex(salt)+Encodes.encodeHex(hashPassword);
+	public static String entryptPassword(String plainPassword,String salt) {
+		byte[] hashPassword = Digests.md5(String.valueOf(plainPassword).concat(
+				String.valueOf(salt)).getBytes());
+		return DatatypeConverter.printBase64Binary(hashPassword);
 	}
 	
 	/**
 	 * 验证密码
 	 * @param plainPassword 明文密码
+	 * @param salt 盐
 	 * @param password 密文密码
 	 * @return 验证成功返回true
 	 */
-	public static boolean validatePassword(String plainPassword, String password) {
-		byte[] salt = Encodes.decodeHex(password.substring(0,SALT_SIZE*2));
-		byte[] hashPassword = Digests.sha1(plainPassword.getBytes(), salt, HASH_INTERATIONS);
-		return password.equals(Encodes.encodeHex(salt)+Encodes.encodeHex(hashPassword));
+	public static boolean validatePassword(String plainPassword, String salt, String password) {
+		return password.equals(entryptPassword(plainPassword,salt));
 	}
 	
 	public static void main(String[] args){
 		
-		String pwd = "admin";
-		
-		System.out.println(entryptPassword(pwd));
+		String pwd = "manager712";
+		String salt = "8o6b0dbbQJDkp9mWDjqeeO";
+		System.out.println(entryptPassword(pwd,salt));
+		System.out.println(validatePassword(pwd, salt, "dy6r5MtDqRBboP8VI7RWCA=="));
 	}
 }
